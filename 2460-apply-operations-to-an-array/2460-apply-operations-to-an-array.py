1f82ffda76108fbbbe30b19efee71f6383dcc395
Nuha Samson <nuhaskingdom@gmail.com>
2026-04-09 15:47:37 +0300

    Time: 0 ms (100%), Space: 19.3 MB (77.08%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -4,11 +4,11 @@ class Solution:
             if nums[i] == nums[i+1]:
                 nums[i] *= 2
                 nums[i+1] = 0 
-                
-        non_zero_pos = 0
+
+        not_zero = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[non_zero_pos], nums[i] = nums[i], nums[non_zero_pos]
-                non_zero_pos += 1
+                nums[not_zero], nums[i] = nums[i], nums[not_zero]
+                not_zero += 1
         
         return nums
