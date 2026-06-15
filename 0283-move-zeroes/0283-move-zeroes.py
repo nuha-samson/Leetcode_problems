235a6ceea3bf0a8680d984c6e98ed90beb50a79f
Nuha Samson <nuhaskingdom@gmail.com>
2026-06-15 19:40:42 +0300

    Time: 7 ms (41.45%), Space: 20.4 MB (61.7%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,9 +4,9 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         slow = 0
-        for fast in range(len(nums)):
-            if nums[fast] != 0:
-                nums[slow] = nums[fast]
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[slow] = nums[i]
                 slow += 1
         while slow < len(nums):
             nums[slow] = 0
