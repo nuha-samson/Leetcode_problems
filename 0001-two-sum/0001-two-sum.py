f56cd2c48640a37d00cdce91d373ce2a784d5a83
Nuha Samson <nuhaskingdom@gmail.com>
2026-08-07 23:28:06 +0300

    Time: 0 ms (100%), Space: 20.5 MB (40.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,11 @@
+from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        check = {}
+        my_dict = defaultdict(int)
         for i in range(len(nums)):
-            if target - nums[i] in check:
-                return [check[target - nums[i]],i]
-            check[nums[i]] = i
+            if target - nums[i]  in my_dict:
+                return my_dict[target - nums[i]],i
+            my_dict[nums[i]] = i
+
+
         
