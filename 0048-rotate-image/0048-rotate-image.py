706eb46eab7d14190daf9c2541aa9db20c53e3c2
Nuha Samson <nuhaskingdom@gmail.com>
2026-08-11 09:11:02 +0300

    Time: 0 ms (100%), Space: 19.3 MB (67.97%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -7,5 +7,6 @@ class Solution:
             for j in range(i,len(matrix)):
                  matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         for i in range(len(matrix)):
-            matrix[i].reverse()
+            for j in range(len(matrix)//2):
+                matrix[i][j],matrix[i][len(matrix)-j-1] = matrix[i][len(matrix)-j-1],matrix[i][j]
         return matrix
