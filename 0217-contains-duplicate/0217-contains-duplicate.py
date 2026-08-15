adba9093f3efa0469459b87b7aee840c4e2ef288
Nuha Samson <nuhaskingdom@gmail.com>
2026-08-15 09:31:33 +0300

    Time: 48 ms (10.86%), Space: 37.1 MB (15.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,9 @@
+from collections import Counter
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashs = {}
-        for i in range(len(nums)):
-            if nums[i] not in hashs:
-                hashs[nums[i]] = 1 + hashs.get(nums[i],0)
-            elif nums[i] in hashs:
+        hashs = Counter(nums)
+        for i in hashs:
+            if hashs[i] >= 2:
                 return True
         return False
         
