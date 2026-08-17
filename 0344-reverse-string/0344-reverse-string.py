e660dfb952a87eac3533071237b4e2f8ef6fa76a
Nuha Samson <nuhaskingdom@gmail.com>
2026-08-17 08:11:06 +0300

    Time: 0 ms (100%), Space: 23.6 MB (19.75%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -6,7 +6,7 @@ class Solution:
         l = 0
         r = len(s) - 1
         while l < r:
-            s[l],s[r] = s[r], s[l]
-            r-=1
+            s[l],s[r] = s[r],s[l]
             l += 1
-        
+            r -= 1
+        return s
